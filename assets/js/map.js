4a649b3283ac5e329b1e1bcd27a9f08fa0193ca1
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-05-01 12:05:13 +0000

    Add function to clear existing markers before adding new ones

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,13 +1,13 @@
 let map;
 
-// function is fired when script is read on index.html
+// initMap function is fired when the google map script is read on index.html
 function initMap() {
     // jQ is the center point
     const jQ = {
         lat: 52.487137777478495,
         lng: -1.9097842445407542
     };
-    // renders map
+    // renders map in #map div
     map = new google.maps.Map(document.getElementById('map'), {
         center: jQ,
         zoom: 14.5,
@@ -16,22 +16,38 @@ function initMap() {
     });    
 }
 
-function addMarker(arr) {
-    let marker = new google.maps.Marker({
-        position: arr.location,
-        map: map,
-        title: arr.name
-    });
-}
-
-function dropMarker(arrayName) {
-    for (let i = 0; i < arrayName.length; i++) {
-        addMarker(arrayName[i]);
+// gets called onclick using the button in the html, where the name of the object is passed
+// clearMarker() is called first to delete any markers that exsist on the map
+// for loop loops through the selected object firing the addMarker function on every loop
+function dropMarker(markersArray) {
+    clearMarker();
+    for (let i = 0; i < markersArray.length; i++) {
+        addMarker(markersArray[i]);
     };
 }
 
-// TEST CONST's
+// gathers info while looping through the object & uses said info to place mark / add info with dot notation
+function addMarker(arr) {
+    let marker = new google.maps.Marker({
+    position: arr.location,
+    map: map,
+    title: arr.name
+    });
+    //pushes info into an array which we then use to delete markers
+    markersArray.push(marker);
+}
 
+let markersArray = [];
+
+// deletes any markers that exsist on the map
+function clearMarker() {
+    for (let i = 0; i < markersArray.length; i++) {
+       markersArray[i].setMap(null); 
+    };
+    markersArray = [];
+}
+
+// TEST CONST's
 const bars = [{location: {lat: 52.4859, lng: -1.9119}, name: 'Button Factory'},
               {location: {lat: 52.4845, lng: -1.9043}, name: 'Cucino Rustica'}
 ];
