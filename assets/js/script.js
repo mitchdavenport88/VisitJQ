525f7633f6e568fb39ee3f1b15a74825d0c1a35f
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-05-03 14:39:32 +0000

    Add function to carousel to hide arrows on index 0 and 3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
-// code for the carousel was found on https://www.youtube.com/watch?v=rTB-UNl_B_Y and edited accordingly
-
+// carousel
+// walkthrough found on https://www.youtube.com/watch?v=rTB-UNl_B_Y and edited accordingly
 const slider = document.querySelector('.slider');
 const leftArrow = document.querySelector('.left');
 const rightArrow = document.querySelector('.right');
@@ -12,6 +12,7 @@ document.querySelectorAll('.carousel-controls li').forEach(function(indicator, i
         document.querySelector('.carousel-controls .selected').classList.remove('selected');
         indicator.classList.add('selected');
         slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+        displayArrow();
     });
 });
 
@@ -20,6 +21,7 @@ leftArrow.addEventListener('click', function() {
     document.querySelector('.carousel-controls .selected').classList.remove('selected');
     indicatorParents.children[sectionIndex].classList.add('selected');
     slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+    displayArrow();
 });
 
 rightArrow.addEventListener('click', function() {
@@ -27,4 +29,21 @@ rightArrow.addEventListener('click', function() {
     document.querySelector('.carousel-controls .selected').classList.remove('selected');
     indicatorParents.children[sectionIndex].classList.add('selected');
     slider.style.transform = 'translate('+ (sectionIndex) * -25 +'%)';
+    displayArrow();
 });
+
+// added function to hide / show arrows depending on index number
+window.onload = displayArrow();
+
+function displayArrow() {
+    if (sectionIndex === 0) {
+        leftArrow.style.display = 'none';
+        rightArrow.style.display = 'block';
+    } else if (sectionIndex !== 0 && sectionIndex !== 3) {
+        leftArrow.style.display = 'block';
+        rightArrow.style.display = 'block';
+    } else if (sectionIndex === 3) {
+        leftArrow.style.display = 'block';
+        rightArrow.style.display = 'none';
+    };
+};
