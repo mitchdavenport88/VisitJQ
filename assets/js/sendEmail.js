bbee7ebaad598ba862b0f70805bd4494e5e786ef
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-05-06 15:23:03 +0000

    Fix bug causing both fail and success messages showing

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,26 +1,27 @@
-// CI walkthrough / EmailJS documentation / Mozilla
+// CI walkthrough / EmailJS documentation
+// idea of showing and hiding alerts came from this Traversy Media video - https://youtu.be/PP4Tr0l08NE
 function sendMail(contactForm) {
     emailjs.send('service_779rxdl', 'template_0i79coo', {
         'user_name': contactForm.user_name.value,
         'user_email': contactForm.user_email.value,
         'message': contactForm.message.value
     })
-    .then(
-        function() {
-            // reset found on https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset
-            document.getElementById('contact-form').reset();
-            // shows success message
-            document.getElementById('success').style.display = 'block';
-            // hides message after 10s
-            setTimeout(function() {
-                document.getElementById('success').style.display = 'none';
-            }, 10000);
-        },
-        function() {
-            // shows failed message
-            document.getElementById('failed').style.display = 'block';
-        }
-    );
+    .then(function() {
+        // hides failed message if / when resubmitted
+        document.getElementById('failed').style.display = 'none';
+        // reset found on https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset
+        document.getElementById('contact-form').reset();
+        // shows success message
+        document.getElementById('success').style.display = 'block';
+        // hides success message after 10s
+        setTimeout(function() {
+            document.getElementById('success').style.display = 'none';
+        }, 10000);
+    },
+    function() {
+        // shows failed message if submission fails
+        document.getElementById('failed').style.display = 'block';    
+    });
     // To block from loading a new page
     return false;
 }
