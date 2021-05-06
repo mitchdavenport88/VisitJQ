8164c3f05b22a8e5e9388dca41afc843a5bf0070
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-05-06 09:34:58 +0000

    Style form in contact section

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,15 +1,20 @@
+// CI walkthrough / EmailJS documentation / Mozilla
 function sendMail(contactForm) {
-    emailjs.send("service_779rxdl","template_0i79coo", {
-        "user_name": contactForm.name.value,
-        "user_email": contactForm.email.value,
-        "message": contactForm.message.value
+    emailjs.send('service_779rxdl', 'template_0i79coo', {
+        'user_name': contactForm.user_name.value,
+        'user_email': contactForm.user_email.value,
+        'message': contactForm.message.value
     })
     .then(
         function(response) {
             console.log('success', response);
+            //reset found on https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset
+            document.getElementById('contact-form').reset();
         },
         function(error) {
             console.log('failed', error);
-        });
+        }
+    );
+    // To block from loading a new page
     return false;
 }
