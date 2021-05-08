f888e8f36e67fdaf171bc8138416498b4c5fcfae
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-05-08 10:00:42 +0000

    Get basic infoWindow to work on marker click

--- assets/js/map.js
+++ assets/js/map.js
@@ -1,7 +1,7 @@
 let map;
 let markersArray = [];
 
-// initMap function is fired when the google map script is read on index.html
+// initMap function is fired when the google map script is read in index.html
 function initMap() {
     // jQ is the center point
     const jQ = {
@@ -17,30 +17,62 @@ function initMap() {
     });
 }
 
-// gets called via the button in the html
-// loops through the selected array firing the addMarker function on every loop
-function dropMarkers(markersArray) {
+// called via eventListeners that pass in the array that holds the objects in map-consts.js
+function dropMarkers(arrayName) {
     clearMarkers();
-    for (let i = 0; i < markersArray.length; i++) {
-        addMarker(markersArray[i]);
+    // calls addMarker on each object / element in the array
+    for (let i = 0; i < arrayName.length; i++) {
+        addMarker(arrayName[i]);
     };
 }
 
-// fired while looping through dropMarkers() / adds marker info to markersArray
-function addMarker(arrayName) {
-    let marker = new google.maps.Marker({
-    position: arrayName.location,
-    map: map,
-    //title: arrayName.name,
-    icon: arrayName.iconImage,
+// adds markers - called while looping through dropMarkers()
+function addMarker(placeObj) {
+    const marker = new google.maps.Marker({
+        position: placeObj.location,
+        map: map,
+        title: placeObj.name,
+        icon: placeObj.iconImage
     });
-    //pushes info into the markersArray which we use to delete/show multiple markers
+    // push marker info to markersArray which we use to show & delete multiple markers
     markersArray.push(marker);
+
+    // following block adds info windows to each marker - walkthrough: Eamonn Smyth, How to Google maps
+    // contentString is the template for each info window - add ${}
+    let contentString = `<h3>window title</h3><p>window info</p><a>window website</a>`;
+    // add info window and its properties
+    const infowindow = new google.maps.InfoWindow({
+        maxWidth: 300,
+        content: contentString
+    });
+
+    // event listener to open info window when marker is clicked
+    marker.addListener('click', function() {
+        //closes windows that are open (seperate function - btm)
+        closeWindows();
+        // opens the window & then sets itself to infoObj - this we clear in closeWindows()
+        infowindow.open(map, marker);
+        infoObj[0] = infowindow;
+    });
+    // stores infowindow info & gets wiped by closeWindows()
+    infoObj = [];
 }
 
-// when called (in dropMarkers) loops through markersArray and and clears it
+// when called (in dropMarkers) loops through markersArray and deletes existing markers
 function clearMarkers() {
+    // removes marker off map
     for (let i = 0; i < markersArray.length; i++) {
-       markersArray[i].setMap(null); 
+       markersArray[i].setMap(null);
+    };
+    // clears array (deletes marker)
+    markersArray = [];
+}
+
+// called when a marker is clicked (addMarker())
+function closeWindows() {
+    // clears infoObj array / removing previously opened window 
+    if (infoObj.length > 0) {
+        infoObj[0].close();
+        infoObj[0].length = 0;
     };
 }
