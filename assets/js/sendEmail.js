9e0559492339660540500c02b141c82f460aaa86
mitchdavenport88 <mitchdavenport88@hotmail.com>
2021-05-10 16:00:58 +0000

    Add contact form validation using JS insead of required feild in tags

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,31 +1,47 @@
-// CI walkthrough / EmailJS documentation
+// authorises EmailJS account - EmailJS documentation
 (function () {
     emailjs.init('user_S3AJosb3HjKtZGHIvYZxa');
 })();
 
-// idea of showing and hiding alerts came from this Traversy Media video - https://youtu.be/PP4Tr0l08NE
-function sendMail(contactForm) {
-    emailjs.send('service_779rxdl', 'template_0i79coo', {
-        'user_name': contactForm.user_name.value,
-        'user_email': contactForm.user_email.value,
-        'message': contactForm.message.value
-    })
-    .then(function() {
-        // hides failed message if / when resubmitted
-        document.getElementById('failed').style.display = 'none';
-        // reset found on https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset
-        document.getElementById('contact-form').reset();
-        // shows success message
-        document.getElementById('success').style.display = 'block';
-        // hides success message after 10s
-        setTimeout(function() {
-            document.getElementById('success').style.display = 'none';
-        }, 10000);
-    },
-    function() {
-        // shows failed message if submission fails
-        document.getElementById('failed').style.display = 'block';    
-    });
-    // To block from loading a new page
-    return false;
-}
+submitButton.addEventListener('click', function () {
+    let userName = document.forms['contact-form']['user_name'].value;
+    let userEmail = document.forms['contact-form']['user_email'].value;
+    let userMessage = document.forms['contact-form']['message'].value;
+    // contact form validation guide on https://www.w3schools.com/js/js_validation.asp
+    if (userName === '' || userEmail === '' || userMessage === '') {
+        document.getElementById('validate').style.display = 'block';
+        setTimeout(function () {
+            document.getElementById('validate').style.display = 'none';
+        }, 6000);
+    };
+    // idea of showing and hiding alerts came from this Traversy Media video - https://youtu.be/PP4Tr0l08NE
+    if (userName !== '' && userEmail !== '' && userMessage !== '') {
+        emailjs.send('service_779rxdl', 'template_0i79coo', {
+                'user_name': userName,
+                'user_email': userEmail,
+                'message': userMessage
+            })
+            .then(function () {
+                // hides failed message if / when resubmitted
+                document.getElementById('failed').style.display = 'none';
+                document.getElementById('validate').style.display = 'none';
+                // reset found on https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/reset
+                document.getElementById('contact-form').reset();
+                // shows success message
+                document.getElementById('success').style.display = 'block';
+                // hides success message after 10s
+                setTimeout(function () {
+                    document.getElementById('success').style.display = 'none';
+                }, 6000);
+            },
+            function () {
+                // shows failed message if submission fails
+                document.getElementById('failed').style.display = 'block';
+                setTimeout(function () {
+                    document.getElementById('failed').style.display = 'none';
+                }, 6000);
+            });
+        // To block from loading a new page
+        return false;
+    };
+});
